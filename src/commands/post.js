95a60310472e6fc1505312948bf5a0fb42382927
Devil Zeldris <Serge7185@yandex.ru>
2023-02-05 22:36:39 +0300

    Some fixes

--- src/commands/post.js
+++ src/commands/post.js
@@ -5,33 +5,35 @@ class MessageEmbedsCommand extends Command {
     }
     async execute(interaction) {
         if (interaction.member.id !== this.ownerID) return interaction.reply({ content: `You dont have permissions` })
-        const { client, options } = interaction
+        const { client, options, channel } = interaction
+        const hook = (await channel.fetchWebhooks()).find(({ owner }) => owner === client.user) || (await channel.createWebhook({ name: `Piety Curse`, avatar: `https://media.discordapp.net/attachments/761694168758091797/1065642907308134470/Piety_Purple-1024.png`, reason: `Someone has been cursed` }))
         const collection = await client.database.collection('embeds')
         await interaction.deferReply({ ephemeral: true })
         if (options.getBoolean('update')) {
-            await this.#update({ interaction, collection })
+            await this.#update({ interaction, collection, hook })
             return interaction.editReply({ content: `Message updated`, ephemeral: true })
         }
-        await this.#put({ interaction, collection })
+        await this.#put({ interaction, collection, hook })
         return interaction.editReply({ content: `Embed has been put into ${interaction.channel.name} chat` })
     }
     async #put({ interaction, collection }) {
-        const message = await collection.findOne({ name: `${interaction.options.getString('name')}` }, { projection: { _id: 0 } })
-        if (!message) return interaction.reply({ content: `No embed found`, ephemeral: true })
+        const embed = await collection.findOne({ name: `${interaction.options.getString('name')}` }, { projection: { _id: 0, roles: 0 } })
+        if (!embed) return interaction.reply({ content: `No embed found`, ephemeral: true })
         const { channel, client } = interaction;
-        const hook = (await channel.fetchWebhooks()).find(({ owner }) => owner === client.user) || (await channel.createWebhook({ name: `Piety Curse`, avatar: `https://media.discordapp.net/attachments/761694168758091797/1065642907308134470/Piety_Purple-1024.png`, reason: `Someone has been cursed` }))
-        const { username, avatarURL, content, embeds, components } = message
-        if (username && avatarURL) return hook.send({ username, avatarURL, content, embeds, components })
-        return interaction.channel.send({ content, embeds, components })
+        const { username, avatarURL, content, embeds, components } = embed
+        if (!username) return interaction.channel.send({ content, embeds, components })
+        return hook.send({ username, avatarURL, content, embeds, components })
     }
-    async #update({ interaction, collection }) {
+    async #update({ interaction, collection, hook }) {
         const { options } = interaction;
         const messageDB = await collection.findOne({ name: `${interaction.options.getString('name')}` }, { projection: { _id: 0 } })
         if (!messageDB) return interaction.reply({ content: `No embed found`, ephemeral: true })
         const messageId = options.getString('message_id');
         const message = await interaction.channel.messages.fetch(messageId)
         const { content, embeds, components } = messageDB
-        return message.edit({ content, embeds, components })
+        if (!message.author.flags) return hook.editMessage(message, { content, embeds, components }).catch(() => undefined)
+        return message.edit({ content, embeds, components }).catch(() => undefined)
+
     }
 }
 module.exports = MessageEmbedsCommand;
