c9e319f23902fb02e25b5ed5aad537dacc1f3535
Devil Zeldris <Serge7185@yandex.ru>
2023-02-05 20:26:52 +0300

    Fix embed command

--- src/commands/post.js
+++ src/commands/post.js
@@ -6,7 +6,7 @@ class MessageEmbedsCommand extends Command {
     async execute(interaction) {
         if (interaction.member.id !== this.ownerID) return interaction.reply({ content: `You dont have permissions` })
         const { client, options } = interaction
-        const collection = await client.database.collection('embeds').catch(() => undefined)
+        const collection = await client.database.collection('embeds')
         await interaction.deferReply({ ephemeral: true })
         if (options.getBoolean('update')) {
             await this.#update({ interaction, collection })
@@ -18,14 +18,14 @@ class MessageEmbedsCommand extends Command {
     async #put({ interaction, collection }) {
         const message = await collection.findOne({ name: `${interaction.options.getString('name')}` }, { projection: { _id: 0 } })
         if (!message) return interaction.reply({ content: `No embed found`, ephemeral: true })
-        const { channel } = interaction;
+        const { channel, client } = interaction;
         const hook = (await channel.fetchWebhooks()).find(({ owner }) => owner === client.user) || (await channel.createWebhook({ name: `Piety Curse`, avatar: `https://media.discordapp.net/attachments/761694168758091797/1065642907308134470/Piety_Purple-1024.png`, reason: `Someone has been cursed` }))
         const { username, avatarURL, content, embeds, components } = message
         if (username && avatarURL) return hook.send({ username, avatarURL, content, embeds, components })
         return interaction.channel.send({ content, embeds, components })
     }
     async #update({ interaction, collection }) {
-        const messageDB = await collection.findOne({ name: `${interaction.options.getString('name')}` }, { projection: { _id: 0 } }).catch(() => undefined)
+        const messageDB = await collection.findOne({ name: `${interaction.options.getString('name')}` }, { projection: { _id: 0 } })
         if (!messageDB) return interaction.reply({ content: `No embed found`, ephemeral: true })
         const message = await interaction.channel.messages.fetch(messageId)
         const messageId = options.getString('message_id');
