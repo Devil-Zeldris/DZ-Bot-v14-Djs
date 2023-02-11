243ff96e2469ae45dc929be4daaab5afdf50bc54
Dolzhanasky <sibiryincs@icloud.com>
2023-02-12 00:20:14 +0200

    chore(env) rename env chore(project) remove PH

--- src/core.js
+++ src/core.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const DevilZeldrisClient = require('./classes/client');
 const { intents, partials } = require('./config.json')
-const client = new DevilZeldrisClient(
-    {
+
+const config = {
         intents,
         partials,
-        token: process.env.token,
-        url: process.env.DBUrl,
+        token: process.env.TOKEN,
+        url: process.env.DB_URL,
         failIfNotExists: false
-    }
-);
+}
+
+const client = new DevilZeldrisClient(config);
 
 client.init()
     .then(() => console.log(`[INIT]`, `All systems initiated`))
